ae21ddc92f944fefe197039bf7175eafa0dde7a6
ushorter <ushorter@gmail.com>
2021-08-11 08:35:53 +0100

    Fixed cube connecting every corner and made it a normal cube. Also added auto rotation.

--- engine.js
+++ engine.js
@@ -7,10 +7,15 @@ var vWidth = window.innerWidth;
 canvas.height = vHeight;
 canvas.width = vWidth;
 
-//DOM elements
+//Inputs (angles)
 var xangleinput = document.getElementById("x");
 var yangleinput = document.getElementById("y");
 var zangleinput = document.getElementById("z");
+
+//Starting values
+xangleinput.value = 3.14;
+yangleinput.value = 0;
+zangleinput.value = 3.14;
 
 //Plane variables
 var screen = [0, 0, -300];
@@ -18,6 +23,7 @@ var screenSize = [100, 100];
 var eye = [0, 0, -500];
 var zn = 500;
 
+//Cube corners coordinates
 var cubeDots = [
   [100, 100, 100],
   [-100, 100, 100],
@@ -29,24 +35,66 @@ var cubeDots = [
   [100, -100, -100],
 ]
 
+//Keeps track of the cube after it's rotation (used to draw the cube only)
 var rotatedCube = [];
 
-//drawLine(coordinator([cubeDots[0][0] * ((zn) / (zn+cubeDots[0][2])), cubeDots[0][1] * ((zn) / (zn+cubeDots[0][2])), 0]), coordinator([cubeDots[1][0] * ((zn) / (zn+cubeDots[1][2])), cubeDots[1][1] * ((zn) / (zn+cubeDots[1][2]))]));
-
+//Connects corners of a cube (this is done manually and can cause problems if coordinates of points are not in order)
 function drawCube(cube){
-  for (var i=0; i<cube.length; i++){
-    for (var j=0; j<cube.length; j++){
-      coordinatedi = coordinator([cube[i][0] * ((zn) / (zn+cube[i][2])), cube[i][1] * ((zn) / (zn+cube[i][2])), 0]);
-      coordinatedj = coordinator([cube[j][0] * ((zn) / (zn+cube[j][2])), cube[j][1] * ((zn) / (zn+cube[j][2])), 0]);
-      drawLine(coordinatedi, coordinatedj)
-    }
-  }
+  coordinatedi = coordinator([cube[0][0] * ((zn) / (zn+cube[0][2])), cube[0][1] * ((zn) / (zn+cube[0][2])), 0]);
+  coordinatedj = coordinator([cube[1][0] * ((zn) / (zn+cube[1][2])), cube[1][1] * ((zn) / (zn+cube[1][2])), 0]);
+  drawLine(coordinatedi, coordinatedj)
+
+  coordinatedi = coordinator([cube[1][0] * ((zn) / (zn+cube[1][2])), cube[1][1] * ((zn) / (zn+cube[1][2])), 0]);
+  coordinatedj = coordinator([cube[2][0] * ((zn) / (zn+cube[2][2])), cube[2][1] * ((zn) / (zn+cube[2][2])), 0]);
+  drawLine(coordinatedi, coordinatedj)
+
+  coordinatedi = coordinator([cube[2][0] * ((zn) / (zn+cube[2][2])), cube[2][1] * ((zn) / (zn+cube[2][2])), 0]);
+  coordinatedj = coordinator([cube[3][0] * ((zn) / (zn+cube[3][2])), cube[3][1] * ((zn) / (zn+cube[3][2])), 0]);
+  drawLine(coordinatedi, coordinatedj)
+
+  coordinatedi = coordinator([cube[3][0] * ((zn) / (zn+cube[3][2])), cube[3][1] * ((zn) / (zn+cube[3][2])), 0]);
+  coordinatedj = coordinator([cube[0][0] * ((zn) / (zn+cube[0][2])), cube[0][1] * ((zn) / (zn+cube[0][2])), 0]);
+  drawLine(coordinatedi, coordinatedj)
+
+  coordinatedi = coordinator([cube[3][0] * ((zn) / (zn+cube[3][2])), cube[3][1] * ((zn) / (zn+cube[3][2])), 0]);
+  coordinatedj = coordinator([cube[7][0] * ((zn) / (zn+cube[7][2])), cube[7][1] * ((zn) / (zn+cube[7][2])), 0]);
+  drawLine(coordinatedi, coordinatedj)
+
+  coordinatedi = coordinator([cube[2][0] * ((zn) / (zn+cube[2][2])), cube[2][1] * ((zn) / (zn+cube[2][2])), 0]);
+  coordinatedj = coordinator([cube[6][0] * ((zn) / (zn+cube[6][2])), cube[6][1] * ((zn) / (zn+cube[6][2])), 0]);
+  drawLine(coordinatedi, coordinatedj)
+
+  coordinatedi = coordinator([cube[1][0] * ((zn) / (zn+cube[1][2])), cube[1][1] * ((zn) / (zn+cube[1][2])), 0]);
+  coordinatedj = coordinator([cube[5][0] * ((zn) / (zn+cube[5][2])), cube[5][1] * ((zn) / (zn+cube[5][2])), 0]);
+  drawLine(coordinatedi, coordinatedj)
+
+  coordinatedi = coordinator([cube[0][0] * ((zn) / (zn+cube[0][2])), cube[0][1] * ((zn) / (zn+cube[0][2])), 0]);
+  coordinatedj = coordinator([cube[4][0] * ((zn) / (zn+cube[4][2])), cube[4][1] * ((zn) / (zn+cube[4][2])), 0]);
+  drawLine(coordinatedi, coordinatedj)
+
+  coordinatedi = coordinator([cube[5][0] * ((zn) / (zn+cube[5][2])), cube[5][1] * ((zn) / (zn+cube[5][2])), 0]);
+  coordinatedj = coordinator([cube[4][0] * ((zn) / (zn+cube[4][2])), cube[4][1] * ((zn) / (zn+cube[4][2])), 0]);
+  drawLine(coordinatedi, coordinatedj)
+
+  coordinatedi = coordinator([cube[5][0] * ((zn) / (zn+cube[5][2])), cube[5][1] * ((zn) / (zn+cube[5][2])), 0]);
+  coordinatedj = coordinator([cube[6][0] * ((zn) / (zn+cube[6][2])), cube[6][1] * ((zn) / (zn+cube[6][2])), 0]);
+  drawLine(coordinatedi, coordinatedj)
+
+  coordinatedi = coordinator([cube[6][0] * ((zn) / (zn+cube[6][2])), cube[6][1] * ((zn) / (zn+cube[6][2])), 0]);
+  coordinatedj = coordinator([cube[7][0] * ((zn) / (zn+cube[7][2])), cube[7][1] * ((zn) / (zn+cube[7][2])), 0]);
+  drawLine(coordinatedi, coordinatedj)
+
+  coordinatedi = coordinator([cube[4][0] * ((zn) / (zn+cube[4][2])), cube[4][1] * ((zn) / (zn+cube[4][2])), 0]);
+  coordinatedj = coordinator([cube[7][0] * ((zn) / (zn+cube[7][2])), cube[7][1] * ((zn) / (zn+cube[7][2])), 0]);
+  drawLine(coordinatedi, coordinatedj);
 }
 
+//Clears the screen
 function clearScreen(){
   cont.clearRect(0, 0, vWidth, vHeight);
 }
 
+//Draw x and y axis
 function drawCenter(){
   cont.strokeStyle = "#333333";
   cont.lineWidth = 3;
@@ -62,6 +110,7 @@ function drawCenter(){
   cont.stroke();
 }
 
+//Draw a dot in a certain coordinates
 function drawDot(coords){
   cont.beginPath();
   coordinated = [coords[0] * ((zn) / (zn+coords[2])), coords[1] * ((zn) / (zn+coords[2])), coords[2]];
@@ -71,6 +120,7 @@ function drawDot(coords){
   cont.fill();
 }
 
+//Draw a line between two points
 function drawLine(p1, p2){
   cont.strokeStyle = "#FFFFFF";
   cont.lineWidth = 3;
@@ -80,94 +130,96 @@ function drawLine(p1, p2){
   cont.stroke();
 }
 
+//Transforms math coordinates into screen coordinates ((0, 0) means the center of the screen)
 function coordinator(coordinates){
   return [vWidth/2+coordinates[0], vHeight/2-coordinates[1], coordinates[2]];
 }
 
-function drawAllSquare(square){
-  console.log("Drawing all square")
-  for (var i=0; i<square.length; i++){
-    for (var j=0; j<square.length; j++){
-      drawLine(coordinator(square[i]), coordinator(square[j]));
-    }
-  }
-}
-
+//Rotates the cube around the x axis
 function rotateX(vector, angle){
   matrix = [Math.cos(angle), -Math.sin(angle), 0, Math.sin(angle), Math.cos(angle), 0, 0, 0, 1];
   return matrixVectorMultiply(matrix, vector);
 }
 
+//Rotates the cube around the y axis
 function rotateY(vector, angle){
   matrix = [Math.cos(angle), 0, Math.sin(angle), 0, 1, 0, -Math.sin(angle), 0, Math.cos(angle)];
   return matrixVectorMultiply(matrix, vector);
 }
 
+//Rotates the cube around the z axis
 function rotateZ(vector, angle){
   matrix = [1, 0, 0, 0, Math.cos(angle), -Math.sin(angle), 0, Math.sin(angle), Math.cos(angle)];
   return matrixVectorMultiply(matrix, vector);
 }
 
-xangleinput.addEventListener("input", function(){
+//Redraw the cube when any of the angles is changed
+function angleChanged(){
   clearScreen();
   drawCenter();
-  //Calculate new dot coordinate
-  //Plot dot with angle
   rotatedCube = [];
   for (var i=0; i<cubeDots.length; i++){
     rotatedCube.push(rotateX(rotateY(rotateZ(cubeDots[i], zangleinput.value), yangleinput.value), xangleinput.value));
-    drawDot(rotateX(rotateY(rotateZ(cubeDots[i], zangleinput.value), yangleinput.value), xangleinput.value));
   }
   drawCube(rotatedCube);
+}
+
+xangleinput.addEventListener("input", function(){
+  angleChanged();
 });
 
 yangleinput.addEventListener("input", function(){
-  clearScreen();
-  drawCenter();
-  //Calculate new dot coordinate
-  //Plot dot with angle
-  rotatedCube = [];
-  for (var i=0; i<cubeDots.length; i++){
-    rotatedCube.push(rotateX(rotateY(rotateZ(cubeDots[i], zangleinput.value), yangleinput.value), xangleinput.value));
-    drawDot(rotateX(rotateY(rotateZ(cubeDots[i], zangleinput.value), yangleinput.value), xangleinput.value));
-  }
-  drawCube(rotatedCube);
+  angleChanged();
 });
 
 zangleinput.addEventListener("input", function(){
-  clearScreen();
-  drawCenter();
-  //Calculate new dot coordinate
-  //Plot dot with angle
-  rotatedCube = [];
-  for (var i=0; i<cubeDots.length; i++){
-    rotatedCube.push(rotateX(rotateY(rotateZ(cubeDots[i], zangleinput.value), yangleinput.value), xangleinput.value));
-    drawDot(rotateX(rotateY(rotateZ(cubeDots[i], zangleinput.value), yangleinput.value), xangleinput.value));
-  }
-  drawCube(rotatedCube);
+  angleChanged();
 });
 
+//Multiplies a vector by a matrix
 function matrixVectorMultiply(matrix, vector){
   result = [
     matrix[0] * vector[0] + matrix[1] * vector[1] + matrix[2] * vector[2],
     matrix[3] * vector[0] + matrix[4] * vector[1] + matrix[5] * vector[2],
     matrix[6] * vector[0] + matrix[7] * vector[1] + matrix[8] * vector[2],
-  ]
+  ];
   return result;
 }
 
-drawCenter();
+//Increments the angles by a certain value every x ms
+var addX = setInterval(increaseX, 10);
+var addY = setInterval(increaseY, 10);
+var addZ = setInterval(increaseZ, 10);
 
-var square = [[-100, 100], [100, 100],
-              [-100, -100], [100, -100]];
-
-//Visualizing the cube in dots
-for (var i=0; i<cubeDots.length; i++){
-  drawDot(cubeDots[i]);
+function increaseX(){
+  if (zangleinput.value >= 3.14){
+    zangleinput.value = -3.14;
+  } else {
+    value = parseFloat(zangleinput.value) + 0.01
+    zangleinput.value = value;
+  }
+  angleChanged();
 }
 
-drawCube(cubeDots);
+function increaseY(){
+  if (yangleinput.value >= 3.14){
+    yangleinput.value = -3.14;
+    console.log(yangleinput.value);
+  } else {
+    yangleinput.value = parseFloat(yangleinput.value) + 0.01;
+  }
+  angleChanged();
+}
 
-canvas.addEventListener("click", function(){
-  console.log(rotatedCube);
-})
+function increaseZ(){
+  if (xangleinput.value >= 3.14){
+    xangleinput.value = -3.14;
+    console.log(xangleinput.value);
+  } else {
+    xangleinput.value = parseFloat(xangleinput.value) + 0.01;
+  }
+  angleChanged();
+}
+
+drawCenter();
+drawCube(cubeDots);
